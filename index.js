194467b37ce279fa22fd28d9d27ff1791640a71d
Anders <andersmurphy@gmail.com>
2016-12-11 13:26:38 +0000

    adds questions.csv and hooks up parser

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import {makeStore} from './src/store'
 import {startServer} from './src/server'
+import {parseQuestionsCSV} from './src/parser'
 
 export const store = makeStore()
 startServer(store)
+
+store.dispatch({
+  type: 'SET_QUESTIONS',
+  questions: parseQuestionsCSV('questions.csv')
+})
+store.dispatch({
+  type: 'SET_MAX_SCORE'
+})
