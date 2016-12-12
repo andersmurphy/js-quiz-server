7a5aab6d1656606745f7eb54018f52564f386758
Anders <andersmurphy@gmail.com>
2016-12-12 18:57:58 +0000

    adds TRY_AGAIN action

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,18 @@
 import {setQuestions, setName, next, selectAnswer, INITIAL_STATE,
   setMaxScore} from './core'
 import {Map} from 'immutable'
+import {parseQuestionsCSV} from './parser'
+
+const questions = parseQuestionsCSV('questions.csv')
+const resetState = setMaxScore(setQuestions(INITIAL_STATE, questions))
 
 const actions = Map({
   'SET_QUESTIONS': (state, action) => setQuestions(state, action.questions),
   'SET_NAME': (state, action) => setName(state, action.name),
   'NEXT': (state, action) => next(state),
   'SELECT_ANSWER': (state, action) => selectAnswer(state, action.answer),
-  'SET_MAX_SCORE': (state, action) => setMaxScore(state)
+  'SET_MAX_SCORE': (state, action) => setMaxScore(state),
+  'TRY_AGAIN': (state, action) => resetState
 })
 
 export default (state = INITIAL_STATE, action) => {
